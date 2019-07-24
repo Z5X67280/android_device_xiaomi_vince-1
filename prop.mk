c1a97e67b6d2c12d2536aa19ddb74d90c28b2c2b
Keternal <Z5X67280@163.com>
2019-07-24 19:05:01 +0800

    vince: prop: Opt Code Style
    Signed-off-by: Keternal <Z5X67280@163.com>

--- prop.mk
+++ prop.mk
@@ -8,29 +8,31 @@ ro.vendor.audio.sdk.fluencetype=fluence
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.mokee.snap \
-persist.camera.dual.camera=0
+persist.camera.dual.camera=0 \
 persist.camera.CDS=off
 
 # GPU
-debug.composition.type=c2d
-debug.mdpcomp.idletime=600
-persist.hwc.mdpcomp.enable=true
-persist.hwc.ptor.enable=true
-debug.enable.sglscale=1
-debug.sf.hw=1
-debug.hwui.renderer=opengl
-debug.egl.hw=1
-debug.cpurend.vsync=false
-debug.sf.disable_hwc=0
-debug.sf.recomputecrop=0
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.composition.type=c2d \
+debug.mdpcomp.idletime=600 \
+persist.hwc.mdpcomp.enable=true \
+persist.hwc.ptor.enable=true \
+debug.enable.sglscale=1 \
+debug.sf.hw=1 \
+debug.hwui.renderer=opengl \
+debug.egl.hw=1 \
+debug.cpurend.vsync=false \
+debug.sf.disable_hwc=0 \
+debug.sf.recomputecrop=0 \
 debug.sf.disable_backpressure=1
 debug.sf.latch_unsignaled=1
-persist.hwc.mdpcomp.enable=true
-persist.hwc.ptor.enable=true
-debug.sf.gpu_comp_tiling=1
-debug.performance.tuning=1
+persist.hwc.mdpcomp.enable=true \
+persist.hwc.ptor.enable=true \
+debug.sf.gpu_comp_tiling=1 \
+debug.performance.tuning=1 \
 video.accelerate.hw=1
 
 # Cam2API/EIS
-persist.camera.HAL3.enabled=1
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.camera.HAL3.enabled=1 \
 persist.camera.eis.enable=1
